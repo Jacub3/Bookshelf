c47d53eec7a3697f8fa15cfe61a70ac7ae0f7ed1
Jacob Elliott <elliott.jt3149@gmail.com>
2025-12-21 20:47:28 -0500

    made the site my own

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,37 +1,15 @@
 import { useState } from 'react'
 import { BookList, type books } from './Components/Buttons'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
   const [book, setBook] = useState<books[]>([])
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div>
+      <h1> Bookshelf </h1>
+      <div className='book set area'>
         <BookList book = {book} setBook = {setBook}></BookList>
       </div>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
