8d3fa833ad2ec12773487f8d8341252dc8254146
Jacob Elliott <elliott.jt3149@gmail.com>
2025-12-23 16:58:43 -0500

    changed my mind on this branch, will use in the future. pipeline is finished however

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,14 +1,36 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { BookList, type books } from './Components/Buttons'
 import './App.css'
 
 function App() {
   const [book, setBook] = useState<books[]>([])
+
+  useEffect (() => {
+    fetch('http://localhost:8080/books')
+      .then(res => res.json())
+      .then(data => {
+        setBook(data);              // Set the data from the data saved in sql
+      })
+      .catch(err => console.error("Had error fetching books", err));
+  }, []);
+
   return (
     <>
       <h1> Bookshelf </h1>
       <div className='book-set-area'>
         <BookList book = {book} setBook = {setBook}></BookList>
+      </div>
+      <div>
+        {book.map((singleBook)=>(
+          <div key = {singleBook.id} className = "book-card">
+            <p>{singleBook.title}</p>
+            <p><strong> Author </strong>{singleBook.author}</p>
+            <div>
+              <p>{singleBook.contents}</p>
+            </div>
+            <small>ID: {singleBook.id}</small>
+          </div>
+        ))}
       </div>
     </>
   )
