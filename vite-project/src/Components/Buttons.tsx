a03aeb912c85bc09114c82538a5d342dc21fe9a6
Jacob Elliott <elliott.jt3149@gmail.com>
2025-12-23 02:30:35 -0500

    completed the book creation

--- vite-project/src/Components/Buttons.tsx
+++ vite-project/src/Components/Buttons.tsx
@@ -14,42 +14,92 @@ interface BookListProps{
     setBook: Dispatch<SetStateAction<books[]>>
 }
 export function BookList({setBook}: BookListProps) {
-    const [bookContents, setBookContents] = useState <books>({id: 0, title: '', contents: '', author: '', created: false})
-    const [text, setText] = useState <string>('');
+    const [, setBookContents] = useState <books>({id: 0, title: '', contents: '', author: '', created: true})
+    const [titleText, setTitleText] = useState <string>('');
+    const [contentsText, setContentsText] = useState <string>('');
+    const [authorText, setAuthorText] = useState <string>('');
     const [isVisible, setIsVisible] = useState <boolean>(true);
+    const [isVisible1, setIsVisible1] = useState <boolean>(true);
+    const [isVisible2, setIsVisible2] = useState <boolean>(true);
 
-    // 
+    // How we get title
     const handleTitle = (event: ChangeEvent<HTMLTextAreaElement>) => {
-        setText(event.target.value)
+        setTitleText(event.target.value);
     }
 
-    const handleSave = () => {
-        setBookContents(prev =>({
-            ...prev,
-            title: text
-        }))
+    const handleTitleSave = () =>{
+        setTitleText(titleText)
     }
 
     const handleKeyDown = (e: KeyboardEvent) =>{
         if (e.key === 'Enter' && !e.shiftKey){
-            handleSave()
+            setIsVisible(!isVisible)
+            handleTitleSave();
         }
     }
 
+    // How we get Contents
+    const handleContents = (event: ChangeEvent<HTMLTextAreaElement>) => {
+        setTitleText(event.target.value);
+    }
+
+    const handleContentsSave = () =>{
+        setContentsText(contentsText)
+    }
+
+    const handleKeyDownContents = (e: KeyboardEvent) =>{
+        if (e.key === 'Enter' && !e.shiftKey){
+            setIsVisible1(!isVisible1)
+            handleContentsSave();
+        }
+    }
+
+    // How we get Author
+    const handleAuthor = (event: ChangeEvent<HTMLTextAreaElement>) => {
+        setAuthorText(event.target.value);
+
+    }
+
+    const handleAuthorSave = () =>{
+        setAuthorText(authorText)
+    }
+
+    const handleKeyDownAuthor = (e: KeyboardEvent) =>{
+        if (e.key === 'Enter' && !e.shiftKey){
+            setIsVisible2(!isVisible2)
+            handleAuthorSave();
+        }
+    }
+
+    // sets default && buttons visible
     const handleBook = () =>{
         setBookContents({
             id: Date.now(),
             title: '',
             contents: '',
             author: '',
-            created: false
+            created: true
         });
+
+        // makes all textboxes visible
         setIsVisible(!isVisible)
+        setIsVisible1(!isVisible1)
+        setIsVisible2(!isVisible2)
+    }
+    
+    // Saves the book
+    const handleSaveButton = () => {
+        setBook(prev =>({
+            ...prev,
+            title: titleText,
+            content: contentsText,
+            author: authorText
+        }))
     }
 
     return(
         <div>
-            <div className = "textarea-container">
+            <div className = "book-container">
                 <button
                     onClick = {handleBook}
                     className = "brownBook" 
@@ -60,10 +110,32 @@ export function BookList({setBook}: BookListProps) {
                 <textarea
                     hidden = {isVisible}
                     placeholder = "Input title here"
-                    value = {text}
+                    value = {titleText}
                     onChange = {handleTitle}
                     onKeyDown = {handleKeyDown}
                 />
+                <textarea
+                    hidden = {isVisible1}
+                    placeholder = "Input contents here"
+                    value = {contentsText}
+                    onChange = {handleContents}
+                    onKeyDown = {handleKeyDownContents}
+                />
+                <textarea
+                    hidden = {isVisible2}
+                    placeholder = "Input author here"
+                    value = {authorText}
+                    onChange = {handleAuthor}
+                    onKeyDown = {handleKeyDownAuthor}
+                />
+            </div>
+            <div>
+                <button
+                    hidden = {!isVisible||!isVisible1||!isVisible2}
+                    onClick={handleSaveButton}
+                    className="saveButton"
+                >
+                </button>
             </div>
         </div>
     );
