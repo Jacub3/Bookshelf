b6c2b48909967627a5220c6587f54ecbc40aa421
Jacob Elliott <elliott.jt3149@gmail.com>
2026-01-06 02:02:29 -0500

    adding in dmg soon

--- Frontend/src/Components/Goblin.tsx
+++ Frontend/src/Components/Goblin.tsx
@@ -5,5 +5,30 @@
  *  - Melle => Bruiser and tank
  *  - Range => Mage and Archer
  *  - Healer
+ * 
+ *  - Level: how proficient the enemy is at killing
+ *  - dmgMod: How much proficiency enemies get (the type of character compared to dmg type)
+ *      example: Mages are proficient with spells
+ *  - 
  *==============================================================================================*/
+import { useState } from 'react'
+export interface goblin{
+    level: number
+    weaponType: string
+    dmgMod: number
+    healthPoints: number
+    name: string
+}
 
+// combat function for dealing with the baddies
+export function Combat({tyleType}: tyle){
+
+    tyleType
+    
+    const [isFighting, setIsFighting] = useState<boolean>(false);
+    const [hp, setHP] = useState<number>(healthPoints)
+
+    return (
+        <div></div>
+    )
+}
